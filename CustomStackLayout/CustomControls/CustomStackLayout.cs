5e01b1afd6c0a25648b6168861f0e71c0ef70c32
Bradley Barker <Bradley.Barker@thoughtware.co.za>
2022-07-13 16:48:32 +0200

    Gradient Rainbow

--- CustomStackLayout/CustomControls/CustomStackLayout.cs
+++ CustomStackLayout/CustomControls/CustomStackLayout.cs
@@ -2,59 +2,59 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace CustomStackLayout.CustomControls
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public class CustomStackLayout : StackLayout
     {
         public CustomStackLayout()
         {
-            var g1 = new GradientStop
-            {
-                Color = Color.Red,
-                Offset = 0.0f,
-            };
+            SetBackground();
+        }
 
-            var g2 = new GradientStop
-            {
-                Color = Color.Orange,
-                Offset = 0.2f,
-            };
+        private void SetBackground()
+        {
+            BackgroundPrideMonth();
+        }
+        private void BackgroundPrideMonth()
+        {
+            var gc = new GradientStopCollection
+                        {
+                            new GradientStop
+                        {
+                            Color = Color.Red,
+                            Offset = 0.0f,
+                        },
+                            new GradientStop
+                        {
+                            Color = Color.Orange,
+                            Offset = 0.2f,
+                        },
+                            new GradientStop
+                        {
+                            Color = Color.Yellow,
+                            Offset = 0.4f,
+                        },
+                            new GradientStop
+                        {
+                            Color = Color.Green,
+                            Offset = 0.6f,
+                        },
+                            new GradientStop
+                        {
+                            Color = Color.Blue,
+                            Offset = 0.8f,
+                        },
+                            new GradientStop
+                        {
+                            Color = Color.Purple,
+                            Offset = 0.9f,
+                        }
+                        };
 
-            var g3 = new GradientStop
-            {
-                Color = Color.Yellow,
-                Offset = 0.4f,
-            };
-
-            var g4 = new GradientStop
-            {
-                Color = Color.Green,
-                Offset = 0.6f,
-            };
-
-            var g5 = new GradientStop
-            {
-                Color = Color.Blue,
-                Offset = 0.8f,
-            };
-
-            var g6 = new GradientStop
-            {
-                Color = Color.Purple,
-                Offset = 0.9f,
-            };
-
-            var gc = new GradientStopCollection();
-            gc.Add(g1);
-            gc.Add(g2);
-            gc.Add(g3);
-            gc.Add(g4);
-            gc.Add(g5);
-            gc.Add(g6);
-
-            var linearGradientBrush = new LinearGradientBrush { GradientStops = gc };
-            this.Background = linearGradientBrush;
+            Background = new LinearGradientBrush { GradientStops = gc };
         }
     }
 }
---
+++ CustomStackLayout/Enums/Types.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStackLayout.Enums
+{
+    public enum Types
+    {
+        Pride
+    }
+}
